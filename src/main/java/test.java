ea65303ca28ffc91c78ba14d016baa45df098936
zongzihao <915637831@qq.com>
2020-07-17 15:00:15 +0800

    modify a

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -10,17 +10,7 @@ import java.math.BigDecimal;
 public class test {
 
     public static void main(String[] args){
-        double a = 1.4;
-        double c = Math.ceil(a);
-        double b = 1.4 / 1;
-
-        if(1 == 1){
-            System.out.println("s");
-        }else if(2 == 2){
-            System.out.println("2");
-        }else{
-            System.out.println("23");
-        }
+        System.out.println("this is branch a");
 
     }
 
