7e2304d13b6229c9be5e90e35818f2fef8a4ee32
zongzihao <915637831@qq.com>
2020-09-25 15:43:15 +0800

    冒泡排序

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,23 @@
+package sort;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @ClassName : BubbleSort
+ * @Description : 冒泡排序
+ * @Author : 宗子豪
+ * @Date: 2020-09-24 12:13
+ */
+
+public class BubbleSort implements Sortable {
+
+    public String name = "冒泡排序";
+
+    @Override
+    public <T> void sort(List<T> list, Comparator<T> comparator) {
+
+
+
+    }
+}
